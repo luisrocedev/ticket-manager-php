b1ae4c501fc67338ae689cdcdd896789e2345c99
Luis Rodriguez <luisrocedev@gmail.com>
2025-05-07 23:49:51 +0200

    1

--- ticketscompra.sql
+++ ticketscompra.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:8889
--- Tiempo de generación: 02-05-2025 a las 12:27:41
+-- Tiempo de generación: 07-05-2025 a las 21:48:23
 -- Versión del servidor: 5.7.44
 -- Versión de PHP: 8.2.20
 
@@ -66,7 +66,7 @@ CREATE TABLE `empresas` (
 --
 
 INSERT INTO `empresas` (`id`, `nombre`, `cif`, `direccion`, `telefono`, `email`, `logo`) VALUES
-(4, 'Empresa Demo S.L.', 'B99999999', 'Calle Falsa 123', '600112233', 'demo@empresa.com', NULL);
+(4, 'Empresa Demo S.L.', 'B99999999', 'Calle Falsa 1234', '600112233', 'demo@empresa.com', '');
 
 -- --------------------------------------------------------
 
@@ -82,6 +82,16 @@ CREATE TABLE `facturas` (
   `estado` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'emitida',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `facturas`
+--
+
+INSERT INTO `facturas` (`id`, `numero_factura`, `ticket_id`, `fecha_emision`, `estado`, `created_at`) VALUES
+(1, 'F-2025-85732', 9, '2025-05-02', 'emitida', '2025-05-02 13:14:12'),
+(2, 'F-2025-04742', 12, '2025-05-02', 'emitida', '2025-05-02 17:04:02'),
+(3, 'F-2025-97451', 12, '2025-05-02', 'emitida', '2025-05-02 17:24:23'),
+(4, 'F-2025-82642', 19, '2025-05-03', 'emitida', '2025-05-03 12:04:04');
 
 -- --------------------------------------------------------
 
@@ -106,8 +116,9 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `codigo`, `nombre`, `descripcion`, `precio`, `iva`, `stock`, `created_at`, `updated_at`) VALUES
-(9, 'X001', 'Teclado Mecánico', 'Teclado gaming retroiluminado', 75.00, 21.00, 50, '2025-05-02 12:21:57', '2025-05-02 12:21:57'),
-(10, 'X002', 'Monitor 24\"', 'Monitor Full HD 144Hz', 180.00, 21.00, 20, '2025-05-02 12:21:57', '2025-05-02 12:21:57');
+(9, 'X001', 'Teclado Mecánico', 'Teclado gaming retroiluminado', 75.00, 21.00, 49, '2025-05-02 12:21:57', '2025-05-03 11:31:06'),
+(10, 'X002', 'Monitor 24\"', 'Monitor Full HD 144Hz', 180.00, 21.00, 20, '2025-05-02 12:21:57', '2025-05-02 12:21:57'),
+(11, 'x08', 'regular carne', 'carne de regular de ternera', 2.00, 10.00, 1979, '2025-05-03 11:58:03', '2025-05-03 12:03:47');
 
 -- --------------------------------------------------------
 
@@ -128,6 +139,22 @@ CREATE TABLE `tickets` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `tickets`
+--
+
+INSERT INTO `tickets` (`id`, `numero_ticket`, `fecha`, `empresa_id`, `cliente_id`, `subtotal`, `iva_total`, `total`, `metodo_pago`, `created_at`) VALUES
+(9, '20250502-2914', '2025-05-02 13:13:51', 4, 6, 75.00, 15.75, 90.75, 'efectivo', '2025-05-02 13:13:51'),
+(10, '20250502-9107', '2025-05-02 16:53:54', 4, 6, 75.00, 15.75, 90.75, 'efectivo', '2025-05-02 16:53:54'),
+(11, '20250502-5775', '2025-05-02 17:03:23', 4, NULL, 75.00, 15.75, 90.75, 'efectivo', '2025-05-02 17:03:23'),
+(12, '20250502-2366', '2025-05-02 17:03:49', 4, 6, 75.00, 15.75, 90.75, 'efectivo', '2025-05-02 17:03:49'),
+(13, '20250502-4165', '2025-05-02 17:14:22', 4, NULL, 0.00, 0.00, 0.00, 'efectivo', '2025-05-02 17:14:22'),
+(14, '20250503-5152', '2025-05-03 11:18:16', 4, NULL, 75.00, 15.75, 90.75, 'efectivo', '2025-05-03 11:18:16'),
+(16, '20250503-5199', '2025-05-03 11:25:32', 4, NULL, 75.00, 15.75, 90.75, 'efectivo', '2025-05-03 11:25:32'),
+(17, '20250503-2292', '2025-05-03 11:31:06', 4, NULL, 75.00, 15.75, 90.75, 'efectivo', '2025-05-03 11:31:06'),
+(18, '20250503-4652', '2025-05-03 12:01:38', 4, NULL, 40.00, 4.00, 44.00, 'efectivo', '2025-05-03 12:01:38'),
+(19, '20250503-4470', '2025-05-03 12:03:47', 4, 6, 2.00, 0.20, 2.20, 'tarjeta', '2025-05-03 12:03:47');
+
 -- --------------------------------------------------------
 
 --
@@ -144,6 +171,21 @@ CREATE TABLE `ticket_items` (
   `iva_importe` decimal(10,2) NOT NULL,
   `total` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `ticket_items`
+--
+
+INSERT INTO `ticket_items` (`id`, `ticket_id`, `producto_id`, `cantidad`, `precio_unitario`, `subtotal`, `iva_importe`, `total`) VALUES
+(1, 9, 9, 1, 75.00, 75.00, 15.75, 90.75),
+(2, 10, 9, 1, 75.00, 75.00, 15.75, 90.75),
+(3, 11, 9, 1, 75.00, 75.00, 15.75, 90.75),
+(4, 12, 9, 1, 75.00, 75.00, 15.75, 90.75),
+(5, 14, 9, 1, 75.00, 75.00, 15.75, 90.75),
+(6, 16, 9, 1, 75.00, 75.00, 15.75, 90.75),
+(7, 17, 9, 1, 75.00, 75.00, 15.75, 90.75),
+(8, 18, 11, 20, 2.00, 40.00, 4.00, 44.00),
+(9, 19, 11, 1, 2.00, 2.00, 0.20, 2.20);
 
 --
 -- Índices para tablas volcadas
@@ -212,31 +254,31 @@ ALTER TABLE `clientes`
 -- AUTO_INCREMENT de la tabla `empresas`
 --
 ALTER TABLE `empresas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `facturas`
 --
 ALTER TABLE `facturas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de la tabla `ticket_items`
 --
 ALTER TABLE `ticket_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Restricciones para tablas volcadas
